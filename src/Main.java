219efd1c57ea7cb8b2f76f3c5f4198ac32c93178
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2025-12-16 23:04:02 -0500

    Ejercicio 3: Crear Funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("***************");
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
-        System.out.println(despedida.generarDespedida("Mundo"));
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
     }
 }
