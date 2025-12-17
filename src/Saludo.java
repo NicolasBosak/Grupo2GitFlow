349bd6e0d377dc37e37d368e08baec70ac9c4808
NicolasBosak <sebastian.casamen@udla.edu.ec>
2025-12-16 20:26:42 -0500

    Corregir error Saludo

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,5 +1,8 @@
 public class Saludo {
     public String generarSaludo(String saludo) {
+
+        if(saludo==null || nombre.isEmpty()){
+            return "Hola, invitado.";;
+        }
         return "Hola " + saludo + "!";
-    };
 }
